9537b4173b2ff4a56c12b81366023281cbae5eca
claramanterola <claramanterola@gmail.com>
2019-10-28 15:17:32 -0300

    Footer y contact form de Julieta

---
+++ footer.php
@@ -0,0 +1,31 @@
+<div class="d-flex bg-dark text-white">
+        <footer class="piedepag row">
+          <article class="sucursales col-12 col-md-4 __artpie">
+            <h4>PRODUCTOS</h4>
+            <p><a href="../raquetas.php">Raquetas</a></p>
+            <p><a href="pelotas.php">Pelotas</a></p>
+            <p><a href="cuerdas.php">Cuerdas</a></p>
+            <p><a href="bolsos.php">Bolsos</a></p>
+            <p><a href="accesorios.php">Accesorios</a></p>
+            <p>Promociones</p>
+          </article>
+
+          <article class="sucursales col-12 col-md-4 __artpie">
+            <h4>AYUDA</h4>
+            <p><a href="login.php"></a>Iniciar sesión</p>
+            <p><a href="register.php">Crear cuenta</a></p>
+            <br>
+            <h4>Contacto</h4>
+            <p>0800-1603/1991</p>
+            <p>Juncal 1044, CABA</p>
+          </article>
+
+          <article class="sucursales col-12 col-md-4 __artpie">
+            <h4><a href="faq.php">CONOCENOS</a></h4>
+            <p>Contactanos</p>
+            <p>Nuestras políticas</p>
+            <p>Noticias</p>
+
+          </article>
+        </footer>
+      </div>
