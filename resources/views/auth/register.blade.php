c19e8bc2ca7f5a0dd60710b44e9301d43265d284
Juan S <juan.stillo1@gmail.com>
2019-12-17 13:26:08 -0300

    formulario de registro con avatar

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
+                    <form method="POST" enctype="multipart/form-data" action="{{ route('register') }}">
                         @csrf
 
                         <div class="form-group row">
@@ -51,6 +51,20 @@
 
                         <div class="form-group row">
                             <label id="province" for="province" class="col-md-4 col-form-label text-md-right"></label>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="file" class="col-md-4 col-form-label text-md-right">Foto de perfil</label>
+
+                            <div class="col-md-6">
+                                <input id="file" type="file" class="form-control @error('file') is-invalid @enderror" name="file" value="{{ old('file') }}" required autocomplete="file">
+
+                                @error('file')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
                         </div>
 
                         <div class="form-group row">
