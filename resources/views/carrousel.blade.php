7c86493c7757df19cb77e9ffc214d2f323f4cea6
claramanterola <claramanterola@gmail.com>
2020-01-14 23:54:27 -0300

    Cambio de imagen en el carrusel

--- resources/views/carrousel.blade.php
+++ resources/views/carrousel.blade.php
@@ -5,31 +5,31 @@
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
   </ol>
 
-  <div class="carousel-item">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
       <img class="d-block w-100" src="/images/766-slide2.gif" alt="Pelotas de tenis">
       <div class="carousel-caption d-none d-md-block">
-        <h3 class="carousel-title"> WOMEN POWER </h3>
+        <h3 class="carousel-title"> WOMAN POWER </h3>
         <p class="carousel-desc"> La nueva linea de Serena Williams </p>
       </div>
     </div>
 
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="/images/766-slide1.jpg" alt="Raqueta de tenis HEAD">
-      <div class="carousel-caption d-none d-md-block">
-        <h3 class="carousel-title"> BABOLAT PURE AERO </h3>
-        <p class="carousel-desc"> Con tecnologia Pure Drive® </p>
-      </div>
-    </div>
-
-    <div class="carousel-item">
-      <img class="d-block w-100" src="/images/766-slide3.jpg" alt="Rafael Nadal">
-      <div class="carousel-caption d-none d-md-block">
-        <h3 class="carousel-title"> RAQUETA EDICION LIMITADA </h3>
-        <p class="carousel-desc"> Rafael Nadal Tour 2020 </p>
-      </div>
+  <div class="carousel-item">
+    <img class="d-block w-100" src="/images/766-slide1.jpg" alt="Raqueta de tenis HEAD">
+    <div class="carousel-caption d-none d-md-block">
+      <h3 class="carousel-title"> BABOLAT PURE AERO </h3>
+      <p class="carousel-desc"> Con tecnologia Pure Drive® </p>
     </div>
   </div>
+
+  <div class="carousel-item">
+    <img class="d-block w-100" src="/images/766-slide3.jpg" alt="Rafael Nadal">
+    <div class="carousel-caption d-none d-md-block">
+      <h3 class="carousel-title"> RAQUETA EDICION LIMITADA </h3>
+      <p class="carousel-desc"> Rafael Nadal Tour 2020 </p>
+    </div>
+  </div>
+</div>
 
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
